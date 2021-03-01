f5e2cc3092bafa2c5127dc749e4aacb6ba0cd0fe
Alex <alexandru.cehan@gmail.com>
2021-03-01 16:16:32 +0200

    Initial commit

---
+++ src/com/luv2code/springdemo/FootballCoach.java
@@ -0,0 +1,12 @@
+package com.luv2code.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FootballCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your freekick 400 times!";
+    }
+}
